6f1a5d3ca2b3bcae071c27241fc59f33060786f6
Timo Mayer <mayer.timo@hotmail.com>
2019-08-07 16:51:24 +0200

    wip added gridfs

--- client/src/views/Post/NewPost.jsx
+++ client/src/views/Post/NewPost.jsx
@@ -21,14 +21,14 @@ class NewPost extends Component {
         <a href="/">Home</a>
         <section id="new-post">
           <h2>New Post</h2>
-          <form method="post" action="/posts">
+          <form method="post" action="/post" enctype="multipart/form-data">
             <label htmlFor="title">Title</label>
             <input type="text" name="title" />
             <label htmlFor="text">Text</label>
             <textarea type="text" name="text"></textarea>
             <label htmlFor="imgs">Images</label>
             <input type="button" value="Upload Images" onClick={this.openFileUploader}/>
-            <input type="file" name="imgs" alt="Post Images" ref="fileUploader" multiple />
+            <input type="file" name="img" alt="Post Images" ref="fileUploader" multiple />
             <div className="btn-group">
               <button className="btn-cancel">Cancel</button>
               <button type="submit" className="btn-submit">Post</button>
--- server.js
+++ server.js
@@ -1,28 +1,71 @@
 const chalk = require('chalk');
 const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 5555;
 
 let db;
-const dbPath = 'mongodb+srv://timo:timo@quotes-cluster-v05ei.azure.mongodb.net/test?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://timo:timo@quotes-cluster-v05ei.azure.mongodb.net/blog-jonas-donia?retryWrites=true&w=majority';
 const mongoClient = require('mongodb').MongoClient;
 
+
+const connection = mongoose.createConnection(mongoURI)
+
+// INIT GFS
+let gfs;
+connection.once('open', () => {
+  // INIT STREAM
+  gfs = Grid(connection.db, mongoose.mongo);
+  gfs.collection('uploads');
+})
+
+// CREATE STORAGE ENGINE
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
 // CONNECT TO DATABASE
-mongoClient.connect(dbPath, (err, client) => {
+/*mongoClient.connect(dbPath, (err, client) => {
   if (err) return console.log(chalk.bgRed(' CONNECTION ERROR '), err);
   db = client.db('blog-jonas-donia');
   app.listen(port, function() {
     console.log(chalk.green('server is running on port ' + port));
   })
-})
+})*/
 
+// MIDLEWARE
+app.listen(port, () => console.log(chalk.green('server is running on port ' + port)))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 
 // POSTS
-
+/*
 app.get('/posts', (req, res) => {
   db.collection('posts').find().toArray((err, result) => {
     res.send({
@@ -48,4 +91,20 @@ app.delete('/posts', (req, res) => {
     console.log(chalk.green('posts deleted'));
     res.redirect('/');
   })
+})*/
+
+app.post('/post', upload.single('img'),(req, res) => {
+  res.json({ file: req.file})
+})
+
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      })
+    }
+    return res.json(files)
+  })
+  
 })
